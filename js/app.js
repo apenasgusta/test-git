eb4b465e5b5ab6c3cb32f7e5d84515cd0fec892f
Gustavo Vittorazzi <gustavosuave1@gmail.com>
2024-12-03 20:09:26 -0300

    comentarios para ajudar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
+//clonando repositorio = git clone link // no power shell
+//git status = ve os status
+//apos uma alteração:
+//git add .
+//git commit -m "oq vc mudou"
+//git log ve alteração
+//git remote = ve o ngc pra mandar o commit
+//git push origin main = envair alteração no git do site
+//tem que aceitar a pessoa la no github
+//git pull origin main = vai biaxar o que ta no site
+
+
 let amigos = [];
 
 function adicionar(){
